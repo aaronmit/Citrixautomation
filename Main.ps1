cdabcd61b36f0095559ed59b685c1a367c28d42d
Jo Himself <33869640+realjohimself@users.noreply.github.com>
2018-11-05 14:06:50 +0000

    Add files via upload

--- Main.ps1
+++ Main.ps1
@@ -4,7 +4,15 @@
 # The script requires a ClientName variable which defines the name of the config file.
 # If the script doesn't find the config file in the same folder as the script then it generates a new one by asking questions.
 
+# Uses modified versions of Bretty's scripts
+# I modified them because Bretty embeds the variables, they now accept parameters.
+
 # Run the script on each component server.
+
+#####################################################################################
+#################################### Parameters #####################################
+#####################################################################################
+# Parameters are declared
 
 Param (
     [Parameter(Mandatory=$True)] 
@@ -12,7 +20,8 @@ Param (
     [string]$DomainName,
     [switch]$IsFatVM,
     [switch]$IsMCS,
-    [string]$WorkerOU,
+# Not doing any domain manipulation in this version.
+#    [string]$WorkerOU,
     [switch]$IsUPM,
     [string]$PathToUPM,
     [string]$BaseDDCName,
@@ -21,20 +30,30 @@ Param (
     [switch]$Office365C2R,
     [string]$PathToSoftware,
     [switch]$IsServerVDA,
-    [String]$SQLServer
+    [String]$SQLServer,
+    [string]$LicenseServer
     )
 
-Start-Transcript ".\Transcript.log"
+#####################################################################################
+#################################### Script Prep ####################################
+#####################################################################################
+# Script prepares variables and validates the config file
+
+Start-Transcript ".\$clientname Transcript.log"
 
 $ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 Set-Location $ScriptDir
 
+[xml]$MetaphorDefaults = (Get-Content "./MetaphorDefaults.xml")
+
 # Set variables For the configuration file
 
 $ConfigFile = ".\$clientname.csv"
 
-# Populate the contents of the config file
+# If the config file exists then the scrpt pulls the variables in here. 
+# Variables can still be Null, the contents of the file can be garbage.
+# Validation will take place later. 
 
 if (test-path $ConfigFile) {
     Write-Host "Config file for $ClientName found. Setting variables from config file."
@@ -43,7 +62,8 @@ if (test-path $ConfigFile) {
     $DomainName = $Config.DomainName
     $IsFatVM = $Config.IsFatVM
     $IsMCS = $Config.IsMCS
-    $WorkerOU = $Config.WorkerOU
+# Not doing any domain manipulation in this version.
+#    $WorkerOU = $Config.WorkerOU
     $IsUPM = $Config.IsUPM
     $PathToUPM = $Config.PathToUPM
     $BaseDDCName = $Config.BaseDDCName
@@ -53,29 +73,40 @@ if (test-path $ConfigFile) {
     $PathToSoftware = $Config.PathToSoftware
     $IsServerVDA = $Config.IsServerVDA
 }
+
+# Oh no! The config file is missing entirely!
+# This will generate a new one.
+
 else {
     Write-Host "Config file is missing. Generating..."
     GenerateConfigFile
 }
 
-# Validate that all values are valid
+# Now is later.
+# The script checks to see if any of the required variables are blank.
+# If the script finds null values it will generate a new config file (strong but probably required). 
 
 Write-host "Validating config file"
-if (!$ClientName -or !$DomainName -or !$IsFatVM -or !$WorkerOU -or !$IsUPM -or !$BaseDDCName -or !$BaseSFName -or !$BaseVDAName -or !$Office365C2R -or !$PathToSoftware -or !$IsServerVDA -or !$SQLServer) {
+if (!$ClientName -or !$DomainName -or !$IsFatVM -or !$IsUPM -or !$BaseDDCName -or !$BaseSFName -or !$BaseVDAName -or !$Office365C2R -or !$PathToSoftware -or !$IsServerVDA -or !$SQLServer) {
     Write-Host "Config file is invalid. Deleting invalid file."
     Remove-Item -Path $ConfigFile | Out-Null
     Write-Host "Generating new config file."
     GenerateConfigFile
 }
+
+# If the variables all contain values then the script considers the config file valid.
+# The script does not set values
+
 else {
     Write-Host "Config file validated successfully."
 }
 
 # Variables for the XenApp Site
 
-$SiteDB = "CTX_Site"
-$MonitDB = "CTX_Monit"
-$LogDB = "CTX_Log"
+$SiteDB = $MetaphorDefaults.Delivery.SiteDB
+$MonitDB = $MetaphorDefaults.Delivery.MonitDB
+$LogDB = $MetaphorDefaults.Delivery.LogDB
+$SiteName = $MetaphorDefaults.Delivery.SiteName
 
 # XML for Office
 
@@ -97,11 +128,11 @@ $OfficeXML = @"
 
 # Fully qualify server names
 
-$DDC1 = $BaseDDCName+"01"+$DomainName
-$DDC2 = $BaseDDCName+"02"+$DomainName
+$DDC1 = $BaseDDCName+"01."+$DomainName
+$DDC2 = $BaseDDCName+"02."+$DomainName
 
-$SF1 = $BaseSFName+"01"+$DomainName
-$SF2 = $BaseSFName+"02"+$DomainName
+$SF1 = $BaseSFName+"01."+$DomainName
+$SF2 = $BaseSFName+"02."+$DomainName
 
 # Create Group Policy Objects
 
@@ -109,10 +140,15 @@ New-GPO -Name "Citrix-User"
 New-GPO -Name "Citrix-Computer"
 New-GPO -Name "Citrix-Loopback"
 
-# Functions to configure each server
+#####################################################################################
+##################################### Functions #####################################
+#####################################################################################
+# Functions to manipulate the config file
+# Functions to configure each server type
+
 
 function GenerateConfigFile {
-    $ColHeaders = "ClientName, DomainName, IsFatVM, IsMCS, WorkerOU, IsUPM, PathToUPM, BaseDDCName, BaseSFName, BaseVDAName, Office365C2R, PathToSoftware, IsServerVDA, SQLServer"
+    $ColHeaders = "ClientName, DomainName, IsFatVM, IsMCS, IsUPM, PathToUPM, BaseDDCName, BaseSFName, BaseVDAName, Office365C2R, PathToSoftware, IsServerVDA, SQLServer"
     Write-Host "Config file for $Clientname not found or invalid, generating new config file."
     new-item $ConfigFile -type file | Out-Null
     $ColHeaders | Out-File $ConfigFile
@@ -129,7 +165,8 @@ function GenerateConfigFile {
         Default {$IsMCS=$false}
     }
 }
-    $WorkerOU = Read-Host -Prompt "Please enter the OU where VDAs reside."
+# Not doing any domain manipulation in this version.
+#    $WorkerOU = Read-Host -Prompt "Please enter the OU where VDAs reside."
     switch (Read-Host "Are we using UPM? y/n") {
         y {$IsUPM=$True}
         n {$IsUPM=$False}
@@ -163,12 +200,13 @@ function GenerateConfigFile {
         Default {$IsServerVDA=$True}
     }
     $SQLServer = Read-Host -Prompt "Please enter the SQL Server name."
-    $Answers = "$ClientName, $DomainName, $IsFatVM, $IsMCS, $WorkerOU, $IsUPM, $PathToUPM, $BaseDDCName, $BaseSFName, $BaseVDAName, $Office365C2R, $PathToSoftware, $IsServerVDA, $SQLServer" 
+    $Answers = "$ClientName, $DomainName, $IsFatVM, $IsMCS, $IsUPM, $PathToUPM, $BaseDDCName, $BaseSFName, $BaseVDAName, $Office365C2R, $PathToSoftware, $IsServerVDA, $SQLServer" 
     $Answers | Out-File $ConfigFile -Append
     Write-Host "Config file for $Clientname generated successfully."    
 }
 
 function DeliveryController {
+    Add-PSSnapin Citrix.*
     
     Set-Location $ScriptDir
     Write-Host "Installing Citrix Delivery Controller."
@@ -179,13 +217,15 @@ function DeliveryController {
     Set-Location $ScriptDir
 
 # Start the Delivery Controller configuration
-Write-Host "Configuring Citrix Delivery Controller."
+    Write-Host "Configuring Citrix Delivery Controller."
 
-Add-PSSnapin Citrix.*
-if ($env:COMPUTERNAME -eq $DDC1) {
-    New-XDSite -LoggingDatabaseName $LogDB -LoggingDatabaseServer $SQLServer -MonitorDatabaseName $MonitDB -MonitorDatabaseServer $SQLServer -SiteDatabaseName $SiteDB -SiteDatabaseServer $SQLServer -SiteName "XD7Site"
+if ($DDC1 -contains $env:COMPUTERNAME) {
+    Write-Host "This server is the first Delivery Controller ($env:COMPUTERNAME)."
+    $DeliveryArgs = "-DatabaseServer $SQLServer -DatabaseName_Site $SiteDB -DatabaseName_Logging $LogDB -DatabaseName_Monitor $MonitDB -DatabaseUser $DBUser -DatabasePassword $DBPass -SiteName $SiteName -LicenseServer $LicenseServer"
+    Invoke-Expression "./Scripts/Bretty/configure_xendesktop_site.ps1 $DeliveryArgs"
 }
 else {
+    Write-Host "This server is the second Delivery Controller ($env:COMPUTERNAME)."
     Add-XDController -AdminAddress localhost -SiteControllerAddress $DDC1
     Set-BrokerSite -TrustRequestsSentToTheXmlServicePort $true
 }
@@ -193,12 +233,14 @@ else {
 }
 
 function StoreFront {
+    Add-PSSnapin Citrix*
+    Set-Location $ScriptDir
     $baseurl = "https://storefront.$DomainName"
     $Farmname = "$ClientName StoreFront"
     $Port = "443"
     $TransportType = "HTTPS"
     $sslRelayPort = "443"
-    $Servers = "$DDC1,$DDC2"
+    $DeliveryServers = "$DDC1,$DDC2"
     $LoadBalance = $true
     $FarmType = "XenDesktop"
     $FriendlyName = "$ClientName StoreFront"
@@ -206,7 +248,8 @@ function StoreFront {
     $SFPathWeb = "/Citrix/StoreWeb"
     $SFPathDA = "/Citrix/StoreDesktopAppliance"
 
-    Set-Location $ScriptDir
+# Install StoreFront
+
     Write-Host "Installing Citrix StoreFront."
     $StoreFrontMedia = "./Files/XenAppMedia/x64/XenDesktop Setup/"
     $StoreFrontArgs = "/Components StoreFront /Configure_firewall /noreboot"
@@ -220,11 +263,13 @@ function StoreFront {
 
     import-module "C:\Program Files\Citrix\Receiver StoreFront\Scripts\ImportModules.ps1"
 
-if ($env:COMPUTERNAME -eq $SF1) {
-    Set-DSInitialConfiguration -hostBaseUrl $baseurl -farmName $Farmname -port $Port -transportType $TransportType -sslRelayPort $sslRelayPort -servers $Servers -loadBalance $LoadBalance -farmType $FarmType -StoreFriendlyName $FriendlyName -StoreVirtualPath $SFPath -WebReceiverVirtualPath $SFPathWeb -DesktopApplianceVirtualPath $SFPathDA
+if ($SF1 -contains $env:COMPUTERNAME) {
+    Write-Host "This server is the first StoreFront Server ($env:COMPUTERNAME)."
+    Set-DSInitialConfiguration -hostBaseUrl $baseurl -farmName $Farmname -port $Port -transportType $TransportType -sslRelayPort $sslRelayPort -servers $DeliveryServers -loadBalance $LoadBalance -farmType $FarmType -StoreFriendlyName $FriendlyName -StoreVirtualPath $SFPath -WebReceiverVirtualPath $SFPathWeb -DesktopApplianceVirtualPath $SFPathDA
 }
 else {
-    
+    Write-Host "This server is the second StoreFront Server ($env:COMPUTERNAME)."
+
 }
 
 }
@@ -265,6 +310,11 @@ Invoke-Expression "./XenDesktopVDASetup.exe $VDAArgs"
 Set-Location $ScriptDir
 
 }
+
+#####################################################################################
+###################################### Execute ######################################
+#####################################################################################
+# Runs functions based on machine name
 
 # Figure out which server we're running on
 
